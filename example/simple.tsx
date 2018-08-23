9428c414ab55c15f149f9c4f27b276190b7da4f0
Matthias Lochbrunner <matthias_lochbrunner@web.de>
2018-08-23 10:49:17 +0200

    Adds simple styling and connection

--- example/simple.tsx
+++ example/simple.tsx
@@ -2,13 +2,13 @@ import * as React from 'react';
 
 import * as ReactDOM from 'react-dom';
 
-import { Editor, Node } from 'react-flow-editor';
+import { Editor, Node, Config } from 'react-flow-editor';
 
-const graph: Node[] = [
+const nodes: Node[] = [
     {
         id: 'Node 1',
         payload: { h1: 'hello' },
-        inputs: [{ id: 'Node 2', name: 'input_1' }],
+        inputs: [{ id: 'Node 2', name: 'input 1' }],
         outputs: [],
         size: {
             width: 100,
@@ -19,7 +19,7 @@ const graph: Node[] = [
         id: 'Node 2',
         payload: { h1: 'world' },
         inputs: [],
-        outputs: [{ id: 'Node 1', name: 'output_1' }],
+        outputs: [{ id: 'Node 1', name: 'output 1' }],
         size: {
             width: 100,
             height: 100
@@ -27,14 +27,19 @@ const graph: Node[] = [
     }
 ];
 
-function resolve(payload: any): JSX.Element {
+function resolver(payload: any): JSX.Element {
     if (payload.type === '') return <h2 />;
     return (
         <h1>{payload.h1}</h1>
     );
 }
 
+const config: Config = {
+    resolver,
+    connectionType: 'bezier'
+};
+
 ReactDOM.render(
-    <Editor resolver={resolve} graph={graph} />,
+    <Editor config={config} nodes={nodes} />,
     document.getElementById('root')
 );
--- src/index.tsx
+++ src/index.tsx
@@ -6,14 +6,14 @@ export interface Size {
     height: number
 }
 
-export type BaseCommons = {
+export type BaseConnection = {
     name: string;
-    id: string;
+    id?: string;
 };
 
-export type BaseInput = BaseCommons & {};
+export type BaseInput = BaseConnection & {};
 
-export type BaseOutput = BaseCommons & {};
+export type BaseOutput = BaseConnection & {};
 
 export interface Node {
     id: string;
@@ -24,12 +24,13 @@ export interface Node {
 }
 
 export interface Config {
-
+    resolver: (payload: any) => JSX.Element;
+    connectionType?: 'bezier' | 'linear';
 }
 
 export interface Props {
-    resolver: (payload: any) => JSX.Element;
-    graph: Node[];
+    config: Config;
+    nodes: Node[];
 }
 
 
@@ -37,8 +38,9 @@ export interface State {
 
 }
 
+type vector2d = { x: number, y: number };
 
-export class Editor<T> extends React.Component<Props, State> {
+export class Editor extends React.Component<Props, State> {
 
     constructor(props: Props) {
         super(props);
@@ -48,14 +50,73 @@ export class Editor<T> extends React.Component<Props, State> {
     render() {
 
         const { props } = this;
+        const { config } = props;
 
-        const nodes = props.graph.map(node =>
-            <div key={node.id} className="node">
-                {props.resolver(node.payload)}
+        const nodeStyle = (pos: vector2d) => ({
+            display: 'inline-block',
+            backgroundColor: '#fafafa',
+            fontFamily: 'Arial',
+            position: 'absolute',
+            top: `${pos.y}px`,
+            left: `${pos.x}px`,
+            border: '1px solid #ddd',
+            borderRadius: '10px'
+        });
+
+        const nodeHeaderStyle = {
+            borderBottom: '1px solid #ddd',
+            padding: '6px',
+            textAlign: 'center',
+            cursor: 'grab',
+            backgroundColor: '#f7f7f7',
+        };
+
+        const nodeBodyStyle = {
+            padding: '10px'
+        };
+
+        const svgStyle = {
+            position: 'absolute',
+            top: '0',
+            left: '0'
+        };
+
+        const posMap = new Map<string, vector2d>();
+        posMap.set('Node 1', { x: 10, y: 10 });
+        posMap.set('Node 2', { x: 220, y: 110 });
+
+        const connection = (start: vector2d, end: vector2d, key: string) => {
+            const stroke = '#ccc';
+            const width = 2;
+            if (config.connectionType === 'bezier') {
+                const sign = Math.sign(end.x - start.x);
+                const a1 = { x: start.x + sign * 100, y: start.y };
+                const a2 = { x: end.x - sign * 100, y: end.y };
+                return <path key={key} d={`M${start.x} ${start.y} C ${a1.x} ${a1.y}, ${a2.x} ${a2.y}, ${end.x} ${end.y}`} stroke={stroke} strokeWidth={width} fill="transparent" />;
+            }
+            else if (config.connectionType === 'linear')
+                return <line key={key} x1={start.x} y1={start.y} x2={end.x} y2={end.y} stroke={stroke} fill="transparent" strokeWidth={width} />
+        };
+
+
+        const nodes = props.nodes.map(node =>
+            <div key={node.id} style={nodeStyle(posMap.get(node.id)) as any} className="node">
+                <div className="node-header" style={nodeHeaderStyle as any}>
+                    {node.id}
+                </div>
+                <div className="node-body" style={nodeBodyStyle}>
+                    {props.config.resolver(node.payload)}
+                </div>
             </div>);
+
+        const connections: { in: string, out: string }[] = props.nodes.reduce((p, s) => [...p, ...s.outputs.map(o => ({ in: s.id, out: o.id }))], []);
+        const connectionsLines = connections.map(conn => connection(posMap.get(conn.in), posMap.get(conn.out), `${conn.in}_${conn.out}`));
 
         return (
             <div className="editor">
+                <svg style={svgStyle as any} width="auto" height="auto" xmlns="http://www.w3.org/2000/svg">
+                    {connectionsLines}
+                </svg>
                 {nodes}
             </div>
         );
